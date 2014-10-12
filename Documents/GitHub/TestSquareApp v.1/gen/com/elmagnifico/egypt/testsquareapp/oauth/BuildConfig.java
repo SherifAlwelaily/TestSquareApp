26e73bc1aa1de6effb267329985bcd107cfee0f5
Soad-El-Toukhy <soad.eltoukhy@yahoo.com>
2014-10-12 02:56:41 +0200

    Initial commit

--- Documents/GitHub/TestSquareApp v.1/gen/com/elmagnifico/egypt/testsquareapp/oauth/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.elmagnifico.egypt.testsquareapp.oauth;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
